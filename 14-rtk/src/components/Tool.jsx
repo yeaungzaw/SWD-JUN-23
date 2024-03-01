bd09c90be9e109d34fcab9c958265d5cec4d4cd5
yeaungzaw <yeaungzaw.learner@gmail.com>
2024-03-01 22:31:41 +0630

    finish rtk day 23

---
+++ 14-rtk/src/components/Tool.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { usePostTodoMutation } from "../store/service/endpoints/todo.endpoints";
+
+const Tool = ({ handleRefatch }) => {
+  const [fun, data] = usePostTodoMutation();
+  const [todo, setTodo] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await fun({
+      text: todo,
+    });
+    setTodo("");
+    handleRefatch();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+          type="text"
+          name="todo"
+          id="todo"
+        />
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Tool;
---
+++ 14-rtk/src/main.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import store from "./store/store";
+import { Provider } from "react-redux";
+
+ReactDOM.createRoot(document.querySelector("#root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
