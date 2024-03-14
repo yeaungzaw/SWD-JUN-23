64f37b51dec0410c4bbcbcab677e7b9c550a024d
yeaungzaw <yeaungzaw.learner@gmail.com>
2024-03-14 14:08:01 +0630

    update

--- 7-react/react-crud/src/components/Container.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Container = ({ children }) => {
-  return (
-    <div className="max-w-[900px] mx-auto py-10 text-gray-800">{children}</div>
-  );
-};
-
-export default Container;
